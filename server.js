ac4c511526042adc9f799141584913d8b1d9f7c7
jinminki <tymikijn@naver.com>
2024-09-08 19:07:57 +0900

    파일 수정

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');  // CORS 패키지 추가
 const http = require('http');
 const socketIo = require('socket.io');
 
@@ -6,14 +7,25 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const port = process.env.PORT || 3000;  // 환경 변수에서 포트를 가져옴
+// CORS 설정
+app.use(cors({
+    origin: 'https://66dd766a4a655e7a7077348d--songjintest.netlify.app',  // 클라이언트 URL 허용
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
-// 기본 라우트 설정
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send('GPS Notification System Server is Running');
 });
 
-// 서버 시작
+app.post('/send-location', (req, res) => {
+    const { lat, lon } = req.body;
+    io.emit('notification', '출입 금지 구역에 진입했습니다!');
+    res.sendStatus(200);
+});
+
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
